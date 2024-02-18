b1b0b5b8d4041f54c8f11460bfca067972115c91
aakgh79 <aakgh79@gmail.com>
2024-02-18 09:59:06 +0330

    add all

---
+++ input.js
@@ -0,0 +1,14 @@
+/*const btn = document.querySelector("toggle-password");
+const passwordE1 = Document.querySelector(".form-control");
+    btn. addEventListener("click",()=>{
+if (passwordE1.type === "password") {
+passwordE1.type="text";
+btn.classList.replace("fa-eye", "fa-eye-slash");
+console.log(btn);
+} else{
+passwordE1.type="password";
+btn.classList.replace("fa-eye-slash", "fa-eye");
+console.log(btn);
+
+}
+    })
